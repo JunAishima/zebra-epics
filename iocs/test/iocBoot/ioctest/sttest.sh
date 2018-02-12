b8fdf10488f1be3b38013dc8398c2359d05cb689
Stu Fisher <stu@q3f.org>
2018-02-12 11:25:12 +0100

    almost...

--- iocs/test/iocBoot/ioctest/sttest.sh
+++ iocs/test/iocBoot/ioctest/sttest.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd "/usr/lib/epics/iocBoot/zebra/test"
+EPICS_HOST_ARCH=`/usr/lib/epics/lib/host`
+
+cd "/usr/lib/epics/iocBoot/zebra/test/bin/${EPICS_HOST_ARCH}"
 export ZEBRAPREFIX="${1:-TESTZEBRA}"
 exec ./test sttest.boot
